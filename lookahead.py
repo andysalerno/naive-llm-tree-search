df31186e73bf125b946d1eb42c66b172e389acf2
andy <andysalerno@gmail.com>
2023-11-25 17:23:56 -0800

    Fix problem by using AutoModelForCausalLM instead

--- lookahead.py
+++ lookahead.py
@@ -18,10 +18,10 @@ class LookAheadSampler:
         self.tokenizer = tokenizer
 
         # how many top tokens to evaluate
-        self.test_top_n_tokens = 2
+        self.test_top_n_tokens = 3
 
         # for each top token, how deep should it be evaluated
-        self.test_depth = 1
+        self.test_depth = 2
 
     def select_next_tokens(
         self,
