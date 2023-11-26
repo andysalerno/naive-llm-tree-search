b9afe97570e97e746f416ce55a7692cdd91bc03e
andy <andysalerno@gmail.com>
2023-11-25 17:23:35 -0800

    Fix problem by using AutoModelForCausalLM instead

--- lookahead.py
+++ lookahead.py
@@ -1,8 +1,7 @@
 from torch import NumberType, Tensor
-from transformers import AutoTokenizer
-from awq import AutoAWQForCausalLM
+from transformers import PreTrainedTokenizer
+from awq.models.base import BaseAWQForCausalLM
 import torch
-import math
 
 
 class _Node:
@@ -14,15 +13,15 @@ class _Node:
 
 
 class LookAheadSampler:
-    def __init__(self, model: AutoAWQForCausalLM, tokenizer: AutoTokenizer):
+    def __init__(self, model: BaseAWQForCausalLM, tokenizer: PreTrainedTokenizer):
         self.model = model
         self.tokenizer = tokenizer
 
         # how many top tokens to evaluate
-        self.test_top_n_tokens = 3
+        self.test_top_n_tokens = 2
 
         # for each top token, how deep should it be evaluated
-        self.test_depth = 2
+        self.test_depth = 1
 
     def select_next_tokens(
         self,
@@ -42,18 +41,23 @@ class LookAheadSampler:
         return best_sequence.removeprefix(current_sequence)
 
     def _evaluate(self, depth: int, max_depth: int, node: _Node):
-        print(f"    evaluating sequence: '{node.text}' with score: {node.score}")
+        spaces = " " * depth
+        print(
+            f"    {spaces}evaluating sequence: '{node.text}' with score: {node.score}"
+        )
 
         if depth > max_depth:
+            print(f"    {spaces}(end of sequence)")
             return (node.score, node.text)
 
         # give more weight to later states:
-        modifier = math.log(depth + 2)
+        # modifier = math.log(depth + 2)
+        modifier = 1
 
         # get the mappings of token: value for the top N next tokens
         next_token_scores = self._get_next_token_scores(node.text)
 
-        max_child_score = -99999
+        max_child_score = -float("inf")
         max_child_sequence = None
 
         for k, v in next_token_scores.items():
@@ -74,8 +78,24 @@ class LookAheadSampler:
 
     def _get_score_for_tokenized_input(self, input_ids, n: int) -> (Tensor, Tensor):
         """Given tokenized text, returns a tuple of (topk_values, topk_indices)"""
-        outputs = self.model.forward(input_ids)
+        print(f"testing ids: {input_ids}")
+        with torch.no_grad():
+            outputs = self.model.forward(input_ids)
         next_token_logits = outputs.logits[:, -1, :]
+
+        comma_token = (
+            self.tokenizer.encode(",", return_tensors="pt", add_special_tokens=False)
+            .squeeze()
+            .item()
+        )
+        print(f"comman token id: {comma_token}")
+        comma_token_score = next_token_logits[:, 28725].item()
+        print(f"comma token id: {28725}, value: {comma_token_score}")
+
+        correct_next_token = self.tokenizer.convert_ids_to_tokens(
+            28725, skip_special_tokens=False
+        )
+        print(f'correct value: "{correct_next_token}"')
 
         topk_values, topk_indices = torch.topk(next_token_logits, n)
         topk_values = topk_values[0]
@@ -97,8 +117,35 @@ class LookAheadSampler:
             text, return_tensors="pt", add_special_tokens=True
         ).to("cuda")
 
+        topk_values, topk_indices = self._get_score_for_tokenized_input(input_ids, 1)
+        print(
+            f"(1 only) topk_values: {topk_values} topk_indices: {topk_indices} for text '{text}'"
+        )
+        topk_values, topk_indices = self._get_score_for_tokenized_input(input_ids, 1)
+        print(
+            f"(1 only) topk_values: {topk_values} topk_indices: {topk_indices} for text '{text}'"
+        )
+        topk_values, topk_indices = self._get_score_for_tokenized_input(input_ids, 1)
+        print(
+            f"(1 only) topk_values: {topk_values} topk_indices: {topk_indices} for text '{text}'"
+        )
         topk_values, topk_indices = self._get_score_for_tokenized_input(
             input_ids, self.test_top_n_tokens
+        )
+        print(
+            f"topk_values: {topk_values} topk_indices: {topk_indices} for text '{text}'"
+        )
+        topk_values, topk_indices = self._get_score_for_tokenized_input(
+            input_ids, self.test_top_n_tokens
+        )
+        print(
+            f"topk_values: {topk_values} topk_indices: {topk_indices} for text '{text}'"
+        )
+        topk_values, topk_indices = self._get_score_for_tokenized_input(
+            input_ids, self.test_top_n_tokens
+        )
+        print(
+            f"topk_values: {topk_values} topk_indices: {topk_indices} for text '{text}'"
         )
 
         topk_tokens = self.tokenizer.convert_ids_to_tokens(
@@ -110,6 +157,10 @@ class LookAheadSampler:
             token.replace("▁", " "): score.item()
             for token, score in zip(topk_tokens, topk_values)
         }
+
+        print(f"    found mapping: {token_score_mapping} for text: '{text}'")
+
+        return token_score_mapping
 
         # token healing: back up by one token, get scores again, but this time for tokens that have the final token as a prefix
         # back up by one:
@@ -138,6 +189,10 @@ class LookAheadSampler:
             last_id, skip_special_tokens=False
         )[-1].replace("▁", " ")
         print(f"        last token from input is: '{last_token_text}'")
+
+        # todo: how do we differentiate between these two scenarios:
+        # 1. the next token is correctly identical to the previous token, e.x. 196 -> 1966
+        # 2. the next token was simply re-selecting the previous token, i.e. notorious -> ious
         tokens_with_prefix = {
             k: v
             for (k, v) in token_score_mapping_prefix.items()
--- run.py
+++ run.py
@@ -1,4 +1,9 @@
-from transformers import set_seed, AutoTokenizer
+from transformers import (
+    set_seed,
+    AutoTokenizer,
+    PreTrainedTokenizer,
+    AutoModelForCausalLM,
+)
 from awq import AutoAWQForCausalLM
 from greedy import GreedySampler
 import torch
@@ -8,8 +13,12 @@ from lookahead import LookAheadSampler
 quant_path = "TheBloke/openchat_3.5-AWQ"
 
 # Load model
-model = AutoAWQForCausalLM.from_quantized(
-    quant_path, fuse_layers=True, return_dict_in_generate=True
+# model = AutoAWQForCausalLM.from_quantized(
+model = AutoModelForCausalLM.from_pretrained(
+    quant_path,
+    return_dict_in_generate=True,
+    use_flash_attention_2=False,
+    device_map="cuda:0",
 )
 model.eval()
 tokenizer = AutoTokenizer.from_pretrained(quant_path, trust_remote_code=True)
@@ -33,42 +42,44 @@ def test_with_strategy(strategy, max_new_tokens: int):
     for _ in range(max_new_tokens):
         print("tokenizing inputs...")
         current_sequence = "".join(inputs)
-        input_ids = tokenizer.encode(
-            current_sequence, return_tensors="pt", add_special_tokens=True
-        ).to("cuda")
+        # input_ids = tokenizer.encode(
+        #     current_sequence, return_tensors="pt", add_special_tokens=True
+        # ).to("cuda")
 
         print("generating next logits...")
         with torch.no_grad():
-            outputs = model.forward(input_ids)
-            next_token_logits = outputs.logits[:, -1, :]
+            # print(f"testing ids: {input_ids}")
+            # outputs = model.forward(input_ids)
+            # next_token_logits = outputs.logits[:, -1, :]
 
-            # TOP = len(next_token_logits[0])
-            TOP = 100
+            # # TOP = len(next_token_logits[0])
+            # TOP = 3
 
-            topk_values, topk_indices = torch.topk(next_token_logits, TOP)
-            topk_values = topk_values[0]
-            topk_indices = topk_indices[0]
+            # topk_values, topk_indices = torch.topk(next_token_logits, TOP)
+            # topk_values = topk_values[0]
+            # topk_indices = topk_indices[0]
+            # print(f"topk_values: {topk_values} topk_indices: {topk_indices}")
 
-            topk_tokens = tokenizer.convert_ids_to_tokens(
-                topk_indices, skip_special_tokens=False
-            )
-            token_score_mapping = {
-                token: score.item() for token, score in zip(topk_tokens, topk_values)
-            }
+            # topk_tokens = tokenizer.convert_ids_to_tokens(
+            #     topk_indices, skip_special_tokens=False
+            # )
+            # token_score_mapping = {
+            #     token: score.item() for token, score in zip(topk_tokens, topk_values)
+            # }
 
-            next_tokens = strategy.select_next_tokens(
-                token_score_mapping, current_sequence
-            )
+            # print(f"original token_score_mapping: {token_score_mapping}")
+
+            next_tokens = strategy.select_next_tokens(None, current_sequence)
 
             # next_tokens = "".join(next_tokens)
             # hack: why is this necessary?
             next_tokens = next_tokens.replace("▁", " ")
 
-            print(f"selected token: {next_tokens}")
+            print(f"selected token: '{next_tokens}'")
 
             inputs.append(next_tokens)
 
-            print(f"next sequence: {inputs}")
+            print(f"next sequence: '{inputs}'")
 
         if next_tokens is []:
             return
@@ -80,8 +91,8 @@ def main():
     set_seed(SEED)
     print(f"set seed to: {SEED}")
 
-    print("Testing with: GreedySampler")
-    test_with_strategy(GreedySampler(), MAX_NEW_TOKENS)
+    # print("Testing with: GreedySampler")
+    # test_with_strategy(GreedySampler(), MAX_NEW_TOKENS)
 
     print("Testing with: LookAheadSampler")
     test_with_strategy(LookAheadSampler(model, tokenizer), MAX_NEW_TOKENS)
